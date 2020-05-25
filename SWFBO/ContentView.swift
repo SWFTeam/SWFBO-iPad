caa66a0a3c6002f321486ac9946191bfdb994dbb
julien-guillan <guillan.julien@live.com>
2020-05-25 18:54:51 +0200

    Initial Commit

---
+++ SWFBO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWFBO
+//
+//  Created by Julien Guillan on 25/05/2020.
+//  Copyright © 2020 Julien Guillan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
